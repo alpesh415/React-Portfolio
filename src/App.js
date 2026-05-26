8a88465354da690ed603d86e068c2576fff3f61b
Abhay <abhaybhoi93@gmail.com>
2026-05-26 11:47:56 +0530

    updated portfolio files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Component/Navbar';
+import About from './Component/About';
+import Home from './Component/Home';
+import Resume from './Component/Resume';
+import Services from './Component/Services';
+import Contact from './Component/Contact';
+import Footer from './Component/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <Navbar/>
+        <Home/>
+        <About/>
+        <Resume/>
+        <Services/>
+        <Contact/>
+        <Footer/>
     </div>
   );
 }
